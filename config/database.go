b714c61966ed66d56ddad5d11afdb588760d98a5
sadek-39 <sadeksltn@gmail.com>
2023-11-28 17:46:07 +0600

    Added DB connection

--- config/database.go
+++ config/database.go
@@ -10,7 +10,8 @@ var DB *gorm.DB
 
 func NewDB(params ...string) {
 	var err error
-	dsn := DBUser + ":" + DBPassword + "@tcp(" + DBHost + ":" + DBPort + ")/" + DBName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := DBUser + ":" + DBPassword + "@tcp(" + DBHost + ":" + DBPort + ")/" +
+		DBName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
--- repository/userRepo.go
+++ repository/userRepo.go
@@ -5,9 +5,8 @@ import (
 	"github.com/sadek-39/echo-sql/models"
 )
 
-var DB = config.GetDBInstance()
-
 func GetUsersFromRepo() ([]models.User, error) {
+	var DB = config.GetDBInstance()
 	var users []models.User
 
 	if err := DB.Find(&users).Error; err != nil {
